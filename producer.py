9386caf0a054c766672e8de9349c6b2a76aa050d
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2021-10-21 17:02:21 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -2,9 +2,10 @@ import pika, os
 
 
 # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
-# url = os.environ.get("CLOUDAMQP_URL", "amqps://zaicmnji:nwq_gWv6gm41TiBaPSc8tywaG-dOFcer@puffin.rmq2.cloudamqp.com/zaicmnji")
+#url = os.environ.get("CLOUDAMQP_URL", "amqps://zaicmnji:nwq_gWv6gm41TiBaPSc8tywaG-dOFcer@puffin.rmq2.cloudamqp.com/zaicmnji")
 #url = "amqps://zaicmnji:nwq_gWv6gm41TiBaPSc8tywaG-dOFcer@puffin.rmq2.cloudamqp.com/zaicmnji"
 url="amqps://localhost"
+url=os.environ.get('AMQP_HOST')
 print(url)
 #params = pika.URLParameters(url)
 params ="localhost"
